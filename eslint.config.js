85a005be0f758af87e7184dde2d6f8ebe7f559f7
kaschtn <kaschtn@gmail.com>
2025-12-02 09:55:08 +0100

    chore: updated eslint config

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+const {
+    defineConfig,
+} = require("eslint/config");
+
+const globals = require("globals");
+const js = require("@eslint/js");
+
+const {
+    FlatCompat,
+} = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+module.exports = defineConfig([{
+    languageOptions: {
+        globals: {
+            ...globals.node,
+            ...globals.mocha,
+        },
+
+        ecmaVersion: 2020,
+        parserOptions: {},
+    },
+
+    extends: compat.extends("eslint:recommended"),
+
+    rules: {
+        "indent": ["error", 4],
+        "no-console": "off",
+
+        "no-unused-vars": ["error", {
+            "ignoreRestSiblings": true,
+            "argsIgnorePattern": "^_",
+        }],
+
+        "no-trailing-spaces": "error",
+        "quotes": ["error", "single"],
+        "semi": ["error", "always"],
+    },
+}]);
